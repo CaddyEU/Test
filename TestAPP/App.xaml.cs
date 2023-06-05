6a2b2fb26e1ef74a7c8f4c720ba380b534456dd9
opilane <opilane@HP-CZC2349HTP>
2023-06-05 11:41:56 +0300

    Add project files.

---
+++ TestAPP/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TestAPP.Services;
+using TestAPP.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestAPP
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
